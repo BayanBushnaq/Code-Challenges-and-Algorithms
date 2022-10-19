4bfd5995fc820eeb166f9d62f365af2f5fe2962e
BayanBushnaq <bayanmohdali@yahoo.com>
2022-10-19 19:14:13 +0000

    lab09

---
+++ python/code_challenges/linkedlist/challenge01/challenge01.py
@@ -0,0 +1,12 @@
+# Write here the code challenge solution
+
+from email import header
+
+
+class Linkedlist:
+    def __init__(self,value):
+        self.value = value
+        self.head= None
+        self.tail = None
+        
+
---
+++ python/code_challenges/linkedlist/challenge01/test_challenge01.py
@@ -0,0 +1,6 @@
+# Write your test here
+import pytest
+from linkedlist.challenge01.challenge01 import Linkedlist
+
+
+
