9630d9bbc1d8e2accf121a82b9baf76c67e0d550
BayanBushnaq <bayanmohdali@yahoo.com>
2022-10-22 20:00:43 +0000

    code09

--- python/code_challenges/linkedlist/challenge01/challenge01.py
+++ python/code_challenges/linkedlist/challenge01/challenge01.py
@@ -1,12 +1,57 @@
 # Write here the code challenge solution
 
-from email import header
+class LinkedListNode:
+    def __init__(self,value,nextNode=None):
+        self.value=value
+        self.nextNode=nextNode
 
 
 class Linkedlist:
-    def __init__(self,value):
-        self.value = value
-        self.head= None
-        self.tail = None
+    def __init__(self,head=None):
+        self.head = head 
+
+    def insert(self,value):
+        node=LinkedListNode(value)
+        if self.head is None:
+            self.head=node
+            return 
+        currentNode = self.head
+        while True:
+            if currentNode.nextNode is None:
+                currentNode.nextNode = node
+                break
+            currentNode=currentNode.nextNode 
+
+    def printLinkedList(self):
+        currentNode= self.head
+        while currentNode is not None:
+            print(currentNode.value,"=>", end = ' ' )
+            currentNode=currentNode.nextNode
+        print("None")
+
+    def deletegivenNode(self,node):
+        self.node = node
+        node.value=node.nextNode
+        node.next= node.nextNode
         
+        
+
+
+
+ll=Linkedlist()
+# ll.printLinkedList()
+
+ll.insert("3")
+# ll.printLinkedList()
+
+ll.insert("44")
+ll.printLinkedList()
+
+ll.insert("55")
+ll.printLinkedList()
+
+ll.deletegivenNode("44")
+ll.printLinkedList
+
+
 
--- python/code_challenges/linkedlist/challenge01/test_challenge01.py
+++ python/code_challenges/linkedlist/challenge01/test_challenge01.py
@@ -2,5 +2,10 @@
 import pytest
 from linkedlist.challenge01.challenge01 import Linkedlist
 
+def test_linkedlist():
+    actual = Linkedlist(5)
+    expected = Linkedlist()
+    assert actual == expected
+
 
 
