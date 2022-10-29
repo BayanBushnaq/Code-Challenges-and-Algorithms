9eb4afe46d44439041cb480127d600fbde5ad3c9
BayanBushnaq <bayanmohdali@yahoo.com>
2022-10-29 18:15:52 +0000

    queue

--- python/code_challenges/stack_queue/challenge01/challenge01.py
+++ python/code_challenges/stack_queue/challenge01/challenge01.py
@@ -1,11 +1,13 @@
 # Write here the code challenge solution
 
 class Queue(object):
+    ''' this class contain 3 4 methods which is resbonsible to implement queue in 2 stacks'''
     def __init__ (self):
         self.stack1=[]
         self.stack2=[]
 
     def push(self,item):
+        '''this method enqueue the item into stack1'''
         for i in range(len(self.stack2)):
                 self.stack1.append(self.stack2.pop())
         self.stack1.append(item)
@@ -13,6 +15,7 @@ class Queue(object):
         
 
     def pop(self):
+        ''' this method resbonsible to dequeue the items from stack1 and dequeue it into stack2'''
         if len(self.stack2) == 0:
             return "The Queue is empty"
         else : 
@@ -21,19 +24,21 @@ class Queue(object):
             return self.stack2.pop()
 
     def peek(self):
+        ''' this method resbonsible to get the top of the stack2'''
         # if len(self.stack1) == 0:
         #     return "The Queue is empty"
         #
         # else : 
-            for i in range(len(self.stack1)):
-                self.stack2.append(self.stack1.pop())
-            return self.stack2[-1]
+        for i in range(len(self.stack1)):
+            self.stack2.append(self.stack1.pop())
+        return self.stack2[-1]
         # for i in range(len(self.stack2)):
             # if i == (len(self.stack2)-1):
                 # return self.stack2[i]
        
 
     def empty(self):
+        ''' this method resbonsible to check if the stack2 is empty or not'''
         #return len(self.stack2)
         if len(self.stack2) >= 1:
             return "The Queue is not empty :)"
